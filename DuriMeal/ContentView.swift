440dace8bb521a6a920e0fef5650ed4606661f3b
namudi <namudongs@gmail.com>
2024-03-13 12:31:15 +0900

    재정생활관 크롤링

--- DuriMeal/ContentView.swift
+++ DuriMeal/ContentView.swift
@@ -11,25 +11,50 @@ import SwiftSoup
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!!")
+        Button(action: {
+            fetchAndPrintMenu()
+        }, label: {
+            Text("식단표 불러오기")
+        })
     }
     
-    let urlAdress = "https://knudorm.kangwon.ac.kr/content/K11"
-    
-    func mealParse() {
-        guard let url = URL(string: urlAdress) else { return }
-        do {
-            let html = try String(contentsOf: url, encoding: .utf8)
-            let doc: Document = try SwiftSoup.parse(html)
-            
-//            let meal: String =
-            
-        } catch Exception.Error(let type, let message) {
-            print(message)
-        } catch {
-            print("error")
+    func fetchAndPrintMenu() {
+        let urlString = "https://knudorm.kangwon.ac.kr/content/K11"
+        
+        if let url = URL(string: urlString) {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                
+                if let data = data, let html = String(data: data, encoding: .utf8) {
+                    print("Parsing Start")
+                    // HTML 파싱
+                    do {
+                        let doc: Document = try SwiftSoup.parse(html)
+                        let days = try doc.select("tr")
+                        
+                        for day in days {
+                            var dayMenus: [String] = []
+                            let menus = try day.select("td")
+                            
+                            for menu in menus {
+                                let menuItems = try menu.html().components(separatedBy: "<br>").filter { !$0.isEmpty }
+                                let cleanedMenuItems = menuItems.compactMap { try? SwiftSoup.clean($0, Whitelist.none()) }
+                                dayMenus.append(contentsOf: cleanedMenuItems)
+                            }
+                            
+                            print(dayMenus) // 출력
+                        }
+                    } catch Exception.Error(let type, let message) {
+                        print("Type: \(type), Message: \(message)")
+                    } catch {
+                        print("error")
+                    }
+                }
+            }.resume()
         }
-
     }
 }
 
