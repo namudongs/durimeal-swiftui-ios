8ed5a61e4aa3afdea585824ce378ef43244348fa
namudi <namudongs@gmail.com>
2024-03-19 13:55:19 +0900

    TabView 추가

--- DuriMeal/Views/ContentView.swift
+++ DuriMeal/Views/ContentView.swift
@@ -14,39 +14,11 @@ struct ContentView: View {
     @State private var showingConfirmationDialog: Bool = false
     @State private var campusName: String = "춘천캠퍼스"
     @State private var selectedDay: String = "월"
-
+    
     private var confirmationDialogMessage: String = "캠퍼스를 선택하세요"
     
     var body: some View {
         VStack {
-            Spacer()
-            HStack {
-                Button(action: { showingConfirmationDialog = true }, label: {
-                    Text(campusName)
-                        .font(.headline)
-                        .fontWeight(.semibold)
-                    Image(systemName: "chevron.down")
-                }).confirmationDialog("",
-                                      isPresented: $showingConfirmationDialog,
-                                      titleVisibility: .hidden,
-                                      actions: {
-                    Button("춘천캠퍼스") { campusName = "춘천캠퍼스" }
-                    Button("삼척캠퍼스") { campusName = "삼척캠퍼스" }
-                    Button("도계캠퍼스") { campusName = "도계캠퍼스" }
-                },
-                                      message: {
-                    confirmationDialogMessage == "" ? nil : Text(confirmationDialogMessage)
-                }
-                )
-                .foregroundColor(.black)
-            }
-            Picker("Flavor",
-                   selection: $selectedPlace) {
-                ForEach(Place.allCases) {
-                    Text($0.description).tag($0)
-                }
-            }.pickerStyle(SegmentedPickerStyle()
-            )
             HStack {
                 let daysOfWeek = ["월", "화", "수", "목", "금", "토", "일"]
                 ForEach(daysOfWeek, id: \.self) { day in
@@ -54,27 +26,41 @@ struct ContentView: View {
                         selectedDay = day
                     }
                     .frame(maxWidth: .infinity)
-                    .foregroundColor(selectedDay == day ? .red : .black) // 선택된 요일에 따라 색상 변경
-                }
+                    .foregroundColor(selectedDay == day ? .red : .black)                }
             }
-            
-            switch selectedPlace {
-            case .sarom:
+            TabView {
                 MealView(meals: viewModel.meals.filter { $0.place == "새롬관" && $0.day == selectedDay})
-            case .eroom:
+                    .tabItem {
+                      Image(systemName: "1.square.fill")
+                      Text("새롬관")
+                    }
                 MealView(meals: viewModel.meals.filter { $0.place == "이룸관" && $0.day == selectedDay})
-            case .chunji:
-                MealView(backgroundColor: .green, meals: viewModel.meals.filter { $0.place == "천지관" && $0.day == selectedDay})
-            case .bakrok:
-                MealView(backgroundColor: .brown, meals: viewModel.meals.filter { $0.place == "백록관" && $0.day == selectedDay})
-            case .duri:
-                MealView(backgroundColor: .blue, meals: viewModel.meals.filter { $0.place == "두리관" && $0.day == selectedDay})
-            }
-
+                    .tabItem {
+                      Image(systemName: "2.square.fill")
+                      Text("이룸관")
+                    }
+                MealView(meals: viewModel.meals.filter { $0.place == "천지관" && $0.day == selectedDay})
+                    .tabItem {
+                      Image(systemName: "3.square.fill")
+                      Text("천지관")
+                    }
+                MealView(meals: viewModel.meals.filter { $0.place == "백록관" && $0.day == selectedDay})
+                    .tabItem {
+                      Image(systemName: "3.square.fill")
+                      Text("백록관")
+                    }
+                MealView(meals: viewModel.meals.filter { $0.place == "크누테리아" && $0.day == selectedDay})
+                    .tabItem {
+                      Image(systemName: "3.square.fill")
+                      Text("크누테리아")
+                    }
+                }
+                .font(.headline)
         }
         .onAppear {
             viewModel.fetchMeals()
         }
+        
     }
 }
 
